a849cb4959c542a91acc0a1e840bca9b12908def
wmalisch <malisch.will@gmail.com>
2021-01-14 18:05:03 -0500

    check if binary tree is balanced using support class for better time complexity. Passthe height data from the bottom p

--- src/checkBalancedTree.java
+++ src/checkBalancedTree.java
@@ -1,7 +1,21 @@
 import java.lang.Math;
+final class TreeInfo{
+    public int height;
+    public boolean balanced;
+
+    public TreeInfo(int height, boolean balanced){
+        this.height = height;
+        this.balanced = balanced;
+    }
+}
+
 
 public class checkBalancedTree {
-
+    /**
+     * Method to get height of a binary tree
+     * @param node
+     * @return height, where height of single node tree is 0, and two node tree is 1
+     */
     public static int height(BinaryTree.Node node){
         if(node == null){
             return -1;
@@ -10,9 +24,40 @@ public class checkBalancedTree {
         }
     }
 
+    /**
+     * Top down recursion
+     * @param root
+     * @return true if tree is balanced
+     */
     public static boolean isBalanced(BinaryTree.Node root){
         if(root == null){ return true; }
+
+        // Check if current node height is balanced, then recurse to children nodes. O(n log n) Time complexity
         return (Math.abs(height(root.left) - height(root.right)) < 2) && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private static TreeInfo isBalancedHelper(BinaryTree.Node root){
+        if(root == null){
+            return new TreeInfo(-1, true);
+        }
+        TreeInfo left = isBalancedHelper(root.left);
+        if(!left.balanced){
+            return new TreeInfo(-1,false);
+        }
+        TreeInfo right = isBalancedHelper(root.right);
+        if(!right.balanced){
+            return new TreeInfo(-1,false);
+        }
+
+        if(Math.abs(left.height - right.height) < 2){
+            return new TreeInfo(Math.max(left.height, right.height) + 1, true);
+        }
+
+        return new TreeInfo(-1,false);
+    }
+
+    public static boolean isBalancedBottomUp(BinaryTree.Node root){
+        return isBalancedHelper(root).balanced;
     }
 
     public static void main(String[] args){
@@ -27,6 +72,6 @@ public class checkBalancedTree {
         tree.insert(110);
         tree.insert(120);
         System.out.println(tree.root.key);
-        System.out.println(isBalanced(tree.root));
+        System.out.println(isBalancedBottomUp(tree.root));
     }
 }
