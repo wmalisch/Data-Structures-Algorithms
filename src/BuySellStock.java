48037258235d4c56023f02c507e5a8e49dbe7227
wmalisch <malisch.will@gmail.com>
2021-01-19 10:53:51 -0500

    Buy sell stock one pass

--- src/BuySellStock.java
+++ src/BuySellStock.java
@@ -3,11 +3,20 @@
  */
 public class BuySellStock {
 
-    public int maxProfit(int[] prices){
-        return 1;
+    public static int maxProfit(int[] prices){
+        int maxProfit = 0, min = prices[0];
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] < min){
+                min = prices[i];
+            }else if(prices[i] - min > maxProfit){
+                maxProfit = prices[i] - min;
+            }
+        }
+        return maxProfit;
     }
 
     public static void main(String[] args){
-
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
     }
 }
