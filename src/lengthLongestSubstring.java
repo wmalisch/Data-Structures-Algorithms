e36c062de7c2dfc2d0f38dc6cb57eee01b7a6150
wmalisch <malisch.will@gmail.com>
2021-01-25 19:25:29 -0500

    Length of longest substring set up

---
+++ src/lengthLongestSubstring.java
@@ -0,0 +1,19 @@
+public class lengthLongestSubstring {
+
+    public static int longestSub(String str){
+        
+        return 0;
+    }
+
+    public static void main(String[] args){
+        String one = "abcabccbb";
+        String two = "bbbbb";
+        String three = "pwwkew";
+        String four = "";
+
+        System.out.println(longestSub(one));
+        System.out.println(longestSub(two));
+        System.out.println(longestSub(three));
+        System.out.println(longestSub(four));
+    }
+}
