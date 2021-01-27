46f5e12fb23b14e4f15228b8d16b1fad927406f0
wmalisch <malisch.will@gmail.com>
2021-01-27 14:53:29 -0500

    Dynamic programming approach to fibonacci seq

--- src/fibSeq.java
+++ src/fibSeq.java
@@ -10,6 +10,17 @@ public class fibSeq {
         }
     }
 
+    public static int fibDynamic(int n){
+        int[] fib = new int[n+1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i = 2; i < n + 1; i++){
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        return fib[n];
+
+    }
+
     public static void main(String[] args){
         int a = 3;
         int b = 8;
