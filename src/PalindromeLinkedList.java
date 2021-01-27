808cf93c11710ac65cf306e68fa5d1404579b178
wmalisch <malisch.will@gmail.com>
2021-01-27 18:02:01 -0500

    Algorithm for is linked list a palindrome using list node

--- src/PalindromeLinkedList.java
+++ src/PalindromeLinkedList.java
@@ -1,8 +1,39 @@
 import java.util.LinkedList;
 import java.util.Stack;
-import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class PalindromeLinkedList {
+
+    public class ListNode
+    {
+        public int key;
+        public ListNode next;
+        public ListNode(int item) { this.key = item; this.next = null; }
+        public ListNode(int item, ListNode node){ this.key = item; this.next = node; }
+    }
+
+    // O(n) space and time using list node
+    public static boolean isPalListNode(ListNode head){
+        List<Integer> arr = new ArrayList<>();
+        while(head != null){
+            arr.add(head.key);
+            head = head.next;
+        }
+
+        int i = 0;
+        int j = arr.size() - 1;
+
+        while( i < j ){
+            if(!arr.get(i).equals(arr.get(j))){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
 
     // O(n) space and time with stack
     public static boolean isPalStack(LinkedList<Integer> head){
